d5095f61834c71facd83e9aa26162df36f08e8c9
WFrancois <wroblewski.francois@gmail.com>
2020-02-05 00:21:07 +0100

    Add blank line before warning

--- core.lua
+++ core.lua
@@ -1655,6 +1655,8 @@ do
 						message = format(L.OUTDATED_GAME_TOOLTIP_EXPIRE_IN_HOURS, expiresInHours)
 					end
 
+					output[i] = " "
+					i = i + 1
 					output[i] = {message, "", 1, 1, 1, 1, 1, 1, false}
 					i = i + 1
 				end
